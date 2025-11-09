359f691bd79a8755bcb63a13230c1812c16341f9
BekzhanK1 <bkimadieff@gmail.com>
2025-11-09 13:03:20 +0500

    add comparing month's mood ratings

--- app/api/v1/routes/analytics.py
+++ app/api/v1/routes/analytics.py
@@ -5,7 +5,7 @@ from sqlmodel import Session
 from app.core.deps import get_current_user
 from app.db.session import get_session
 from app.models.user import User
-from app.services.analytics_service import AnalyticsService
+from app.services.analytics_service import analytics_service
 
 
 router = APIRouter()
@@ -20,7 +20,6 @@ def get_data_points_for_mood_trend(
     current_user: User = Depends(get_current_user),
     session: Session = Depends(get_session)
 ):
-    analytics_service = AnalyticsService()
     return analytics_service.get_data_points_for_mood_trend(
         session,
         current_user.id,
@@ -37,5 +36,22 @@ def get_main_themes(
     current_user: User = Depends(get_current_user),
     session: Session = Depends(get_session)
 ):
-    analytics_service = AnalyticsService()
     return analytics_service.get_main_themes(session, current_user.id, start_date, end_date)
+
+
+@router.get("/best-and-worst-day")
+def get_best_and_worst_day(
+    start_date: Optional[date] = None,
+    end_date: Optional[date] = None,
+    current_user: User = Depends(get_current_user),
+    session: Session = Depends(get_session)
+):
+    return analytics_service.get_best_and_worst_entries_by_mood_rating(session, current_user.id, start_date, end_date)
+
+
+@router.get("/compare-current-and-previous-month-mood-rating")
+def compare_current_and_previous_month_mood_rating(
+    current_user: User = Depends(get_current_user),
+    session: Session = Depends(get_session)
+):
+    return analytics_service.compare_current_and_previous_month_mood_rating(session, current_user.id)
